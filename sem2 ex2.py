51d4eada5b9bb50fa689685c4fc9e4c99438caff
barrypearseburke <barry.burke3@mydit.ie>
2015-01-29 23:25:04 +0000

    clock

--- sem2 ex2.py
+++ sem2 ex2.py
@@ -1,7 +1,7 @@
 __author__ = 'Barry'
 
 class clock:
-    def __init__(self, hour,minute,second):
+    def __init__(self, hour =0 ,minute =0 ,second =0):
         self.hour =hour
         self.minute =minute
         self.second =second
@@ -10,18 +10,27 @@ class clock:
     def __str__(self):
         return "{}:{}:{}".format(self.hour,self.minute,self.second)
 
-    def repr(self):
-        self.__str__()
+    def __repr__(self):
+        return self.__str__()
 
+    def addtime(self,hour , minute, second):
+        if (hour <=-1):
+            print("error")
+        return
 
 
 
 def main():
-    hour =1
-    minute =30
-    second =10
-    myclock =clock(hour,minute,second)
+
+    myclock =clock()
     print(myclock)
-    # myclock()
+    #myclock()
+
+    houradd =input("Please enter the current hour")
+    minuteadd = input("Please enter the current minute")
+    secondadd =input(" please enter the current second")
+
+    myclock.addtime(houradd,minuteadd,secondadd)
+
 if __name__ == "__main__":
     main()
