f4ecd9c860aa24273383693094cb2b4e7a9b8733
barrypearseburke <barry.burke3@mydit.ie>
2015-02-07 20:18:36 +0000

    moving block

--- pygameSnakegame.py
+++ pygameSnakegame.py
@@ -16,21 +16,43 @@ pygame.display.set_caption('Snake')
 #pygame.display.update()
 
 gameExit =False
+clock =pygame.time.Clock()
+fps =10
 lead_x =300
-lead_y -300
+lead_y =300
+lead_y_change =0
+lead_x_change =0
+
 while not gameExit:
     for event in pygame.event.get():
         #print(event)
         if event.type == pygame.QUIT:
             gameExit =True
+
+        if event.type ==  pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x_change -= 10
+
+            if event.key == pygame.K_RIGHT:
+                lead_x_change  += 10
+
+        if event.type ==  pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                lead_y_change -= 10
+
+            if event.key == pygame.K_DOWN:
+                lead_y_change  += 10
+    lead_x+= lead_x_change
+    lead_y += lead_y_change
     screen.fill(white)
     #pygame.draw.rect(screen,black,[400,300,10,100]) #where,colour, in list coriates (top left, width,height) is the parameters
     #pygame.draw.rect(screen,red,[400,300,10,10]) #where,colour, in list coriates (top left, width,height) is the parameters
 
     #use fill to draw rect
-    screen.fill(red,rect =[400,300,10,100]) # alterative too draw rect #fill can be graphicly accelrated
+    screen.fill(red,rect =[lead_x,lead_y,10,10]) # alterative too draw rect #fill can be graphicly accelrated
 
     pygame.display.update()
+    clock.tick(fps)
 pygame.quit()
 quit()
 
