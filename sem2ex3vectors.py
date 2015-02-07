1ba3d81c681f84c084d1d07b08354b77754aea0a
barrypearseburke <barry.burke3@mydit.ie>
2015-02-07 15:02:16 +0000

    python

---
+++ sem2ex3vectors.py
@@ -0,0 +1,33 @@
+__author__ = 'Barry'
+#sem2 ex3 vectors
+
+class Vector:
+    def __init__(self,x,y):
+        self.x =x
+        self.y=y
+
+
+    def __str__(self):
+
+
+    def __add__(self, other):
+        pass
+    def __sub__(self, other):
+        pass
+    def __mul__(self, other):
+        pass
+    def magnitude(self,other):
+        pass
+
+# def main():
+#     myvector =vectors
+#     #main area
+#     vectorx1= input("please enter the x1 value")
+#     vectorsy1 =input("please enter the y1 value")
+#
+#     option =
+
+
+
+# if __name__ == "__main__":
+#     main()
