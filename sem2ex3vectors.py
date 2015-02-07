12e9f9fa763d8ae238ad0044967e13756f604324
barrypearseburke <barry.burke3@mydit.ie>
2015-02-07 16:20:52 +0000

    errors

--- sem2ex3vectors.py
+++ sem2ex3vectors.py
@@ -4,16 +4,19 @@ __author__ = 'Barry'
 class Vector:
     def __init__(self,vector1,vector2):
         print(vector1,vector2)
+        self.v1 =vector1
+        self.v2=vector2
 
 
     def __str__(self,result):
         pass #pass for now
         #return result
 
-    def __add__(self, other):
-        pass
+    def __add__(self):
+        (self.v1[1] +self.v2[1]#self.v1[2] + self.v2[2])
+
     def __sub__(self, other):
-        pass
+        return v
     def __mul__(self, other):
         pass
     def magnitude(self,other):
@@ -33,7 +36,7 @@ def main():
 
     Myvector =Vector(vector1,vector2)
 
-
+    add = Vector.__add__(self)
 
 if __name__ =="__main__":
     main()
