8b98b36e2565b579e9c2dd1a76e4abe42db52b9a
barrypearseburke <barry.burke3@mydit.ie>
2015-02-08 19:56:02 +0000

    Text to screen working

--- pygameSnakegame.py
+++ pygameSnakegame.py
@@ -1,6 +1,7 @@
 __author__ = 'Barry'
 
 import pygame
+import  time
 pygame.init()
 
 white = (255,255,255)
@@ -18,12 +19,20 @@ pygame.display.set_caption('Snake')
 gameExit =False
 clock =pygame.time.Clock()
 fps =30
-lead_x =300
-lead_y =300
+lead_x = width/2
+lead_y  = height/2
 lead_y_change =0
 lead_x_change =0
+speed =10
+blockw = 10
+blockh = 10
+font =pygame.font.SysFont(None,25)
+def Message_to_user(msg,colour):
+    screen_text = font.render(msg,True,colour)
+    screen.blit(screen_text, [width/2 , height/2])
 
 while not gameExit:
+
     for event in pygame.event.get():
         #print(event)
         if event.type == pygame.QUIT:
@@ -31,21 +40,21 @@ while not gameExit:
 
         if event.type ==  pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change -= 10
+                lead_x_change -= speed
                 lead_y_change =0
 
             elif event.key == pygame.K_RIGHT:
-                lead_x_change  += 10
+                lead_x_change  += speed
                 lead_y_change =0
 
 
 
             elif event.key == pygame.K_UP:
-                lead_y_change -= 10
+                lead_y_change -= speed
                 lead_x_change =0
 
             elif event.key == pygame.K_DOWN:
-                lead_y_change  += 10
+                lead_y_change  += speed
                 lead_x_change =0
 
             #hold and press  button
@@ -55,6 +64,9 @@ while not gameExit:
             #     if event.key == pygame.K_UP or pygame.K_DOWN :
             #         lead_y_change =0
 
+    if (lead_x >= width or lead_x <= 0) or (lead_y >= height or lead_y <=0):
+        gameExit = True
+
     lead_x+= lead_x_change
     lead_y += lead_y_change
     screen.fill(white)
@@ -62,10 +74,14 @@ while not gameExit:
     #pygame.draw.rect(screen,red,[400,300,10,10]) #where,colour, in list coriates (top left, width,height) is the parameters
 
     #use fill to draw rect
-    screen.fill(red,rect =[lead_x,lead_y,10,10]) # alterative too draw rect #fill can be graphicly accelrated
+    screen.fill(red,rect =[lead_x,lead_y,blockw,blockh]) # alterative too draw rect #fill can be graphicly accelrated
 
     pygame.display.update()
     clock.tick(fps)
+Message_to_user("you lose",black)
+pygame.display.update()
+time.sleep(2)
+
 pygame.quit()
 quit()
 
